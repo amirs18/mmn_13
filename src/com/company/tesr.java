48df74b3e94ec106b2b698af000003a2c1353f1d
Amir <amir.shafat1@gmail.com>
2021-12-09 01:22:49 +0200

    test

--- src/com/company/tesr.java
+++ src/com/company/tesr.java
@@ -8,13 +8,11 @@ public class tesr {
     public static void main(String[] args) {
         Square3x3 square3x3 = new Square3x3();
         Boolean[] booleans = new Boolean[9];
-        for (int i=0;i<booleans.length;i++)
-            booleans[i]=false;
+        Arrays.fill(booleans, false);
 
-        square3x3.whosThereCol(1,booleans);
+        square3x3.whosThereCol(2,booleans);
         System.out.println(square3x3.toString() + square3x3.allThere());
-        for (int i=0;i<booleans.length;i++)
-            System.out.print(booleans[i]+" ");
+        for (Boolean aBoolean : booleans) System.out.print(aBoolean + " ");
 
     }
 }
